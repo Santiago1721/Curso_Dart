137e8e7aea92d8c9dc077cb0248d2876a1ce916c
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2024-05-04 11:06:05 +0200

    Create 13_try_catch_on.dart

---
+++ 13_try_catch_on.dart
@@ -0,0 +1,26 @@
+void main() async {
+  print('Inicio del programa');
+
+  try {
+    final value = await httpGet('https://fernando-herrera.com');
+    print('exito: $value');
+  } on Exception {
+    print('Tenemos una Exception');
+  } catch (err) {
+    print('Ops, algo terrible paso: $err');
+  } finally {
+    print('fin del try y catch');
+  }
+
+  print('fin del programa');
+}
+
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1));
+
+  throw Exception('No hay parametros en el url');
+ 
+  //  return 'respuesta a la peticion http';
+}
+
+  
